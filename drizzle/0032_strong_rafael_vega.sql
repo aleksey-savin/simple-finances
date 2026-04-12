16bd50b1c258bcc290fc13175135e30e1c335085
Aleksey Savin <aleksey.savin@protonmail.com>
2026-04-12 22:11:24 +1000

    fixing migrations

--- drizzle/0032_strong_rafael_vega.sql
+++ drizzle/0032_strong_rafael_vega.sql
@@ -1,1 +1,31 @@
--- placeholder
+CREATE TYPE "public"."contract_type" AS ENUM('customer', 'supplier');
+--> statement-breakpoint
+CREATE TABLE "business_line" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"created_by" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "contract" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"number" text NOT NULL,
+	"signed_at" date NOT NULL,
+	"contract_type" "contract_type" NOT NULL,
+	"amount" numeric NOT NULL,
+	"business_line_id" text NOT NULL,
+	"created_by" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"file_url" text
+);
+--> statement-breakpoint
+ALTER TABLE "business_line" ADD CONSTRAINT "business_line_created_by_user_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+ALTER TABLE "contract" ADD CONSTRAINT "contract_business_line_id_business_line_id_fk" FOREIGN KEY ("business_line_id") REFERENCES "public"."business_line"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+ALTER TABLE "contract" ADD CONSTRAINT "contract_created_by_user_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+CREATE INDEX "contract_business_line_idx" ON "contract" USING btree ("business_line_id");
--- drizzle/0035_clever_lake.sql
+++ drizzle/0035_clever_lake.sql
@@ -1,1 +1,1 @@
--- placeholder
+ALTER TABLE "contract" ALTER COLUMN "amount" TYPE numeric[] USING ARRAY[amount];
